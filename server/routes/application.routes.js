51c6ac00896d7994d309d73dac4e407e3ea4d86c
Sumit Kumar <sumitku2938k@gmail.com>
2026-07-08 16:51:12 +0530

    feat: add route to retrieve application by ID

--- server/routes/application.routes.js
+++ server/routes/application.routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const protect = require('../middleware/auth.middleware');
 const authorize = require('../middleware/role.middleware');
-const { applyForJob, getMyApplications } = require('../controllers/application.controller');
+const { applyForJob, getMyApplications, getApplicationById } = require('../controllers/application.controller');
 
 const router = express.Router();
 
 // Routes for /api/applications
 router.post('/apply/:jobId', protect, authorize('jobSeeker'), applyForJob);
 router.get('/my', protect, authorize('jobSeeker'), getMyApplications);
+router.get('/:id', protect, authorize('jobSeeker'), getApplicationById);
 
 module.exports = router;
