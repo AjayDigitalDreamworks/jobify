92322add2a2d81991a5f450c17eba8d839d94e18
Sumit Kumar <sumitku2938k@gmail.com>
2026-07-13 19:25:10 +0530

    feat: add route to withdraw application

--- server/routes/application.routes.js
+++ server/routes/application.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const protect = require('../middleware/auth.middleware');
 const authorize = require('../middleware/role.middleware');
-const { applyForJob, getMyApplications, getApplicationById, updateApplicationStatus } = require('../controllers/application.controller');
+const { applyForJob, getMyApplications, getApplicationById, updateApplicationStatus, withdrawApplication } = require('../controllers/application.controller');
 
 const router = express.Router();
 
@@ -10,5 +10,6 @@ router.post('/apply/:jobId', protect, authorize('jobSeeker'), applyForJob);
 router.get('/my', protect, authorize('jobSeeker'), getMyApplications);
 router.get('/:id', protect, authorize('jobSeeker'), getApplicationById);
 router.patch('/:id/status', protect, authorize('recruiter'), updateApplicationStatus);
+router.delete('/:id', protect, authorize('jobSeeker'), withdrawApplication);
 
 module.exports = router;
